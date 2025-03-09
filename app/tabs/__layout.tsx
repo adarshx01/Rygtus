b6c3e0d04e053acf827a35a8f80659648c6b7966
Barash sharma <barash1311@gmail.com>
2025-03-09 09:35:49 +0530

    update

--- app/tabs/__layout.tsx
+++ app/tabs/__layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../HomeScreen';
-import VisionBot from '../VisionBot';
-import TalkingBot from '../3DBot';
-import MedBot from '../MedBot';
-import SettingsScreen from '../Settings';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../HomeScreen";
+import VisionBot from "../VisionBot";
+import TalkingBot from "../3DBot";
+import MedBot from "../MedBot";
+import SettingsScreen from "../Settings";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,28 +14,31 @@ export default function Tabs({ userType }) {
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarInactiveTintColor: "grey",
+        tabBarActiveTintColor: "blue",
         tabBarIcon: ({ color, size }) => {
-          let iconName = '';
-          if (route.name === 'HomeScreen') {
-            iconName = 'home';
-          } else if (route.name === 'VisionBot') {
-            iconName = 'chatbubble';
-          } else if (route.name === '3DBot') {
-            iconName = 'camera';
-          } else if (route.name === 'MedBot') {
-            iconName = 'medkit';
-          } else if (route.name === 'Settings') {
-            iconName = 'settings';
+          let iconName = "";
+          if (route.name === "HomeScreen") {
+            iconName = "home";
+          } else if (route.name === "VisionBot") {
+            iconName = "chatbubble";
+          } else if (route.name === "3DBot") {
+            iconName = "camera";
+          } else if (route.name === "MedBot") {
+            iconName = "medkit";
+          } else if (route.name === "Settings") {
+            iconName = "settings";
           }
           return <Ionicons name={iconName} size={size} color={color} />;
         },
       })}
     >
       <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      {userType === 'doctor' && (
+      {userType === "doctor" && (
         <Tab.Screen name="VisionBot" component={VisionBot} />
       )}
-      {userType === 'patient' && (
+      {userType === "patient" && (
         <Tab.Screen name="3DBot" component={TalkingBot} />
       )}
       <Tab.Screen name="MedBot" component={MedBot} />
